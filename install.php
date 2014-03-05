2228ad5d5d1c26ebaac029ae27c42340ff952f4a
aldus <info@drp-x-work.de>
2014-03-05 21:31:11 +0100

    Bugfix inside install.php for WYSIWYG-Admin.

--- install.php
+++ install.php
@@ -26,5 +26,46 @@ if (defined('WB_PATH')) {
 	}
 }
 // end include class.secure.php
+
+/**
+ *	WYSIWYG-Admin
+ */
  
+/**
+ *	Looking for the WYSIWYG-Admin table in the db.
+ */
+
+$ignore = TABLE_PREFIX;
+$all_fields = $database->list_tables( $ignore );
+		
+if (true == in_array("mod_wysiwyg_admin", $all_fields)) {
+	
+	$table = TABLE_PREFIX."mod_wysiwyg_admin";
+	
+	$query = "SELECT `id`,`skin`,`menu`,`height`,`width` from `".$table."` where `editor`='ckeditor_4'limit 0,1";
+	$result = $database->query ($query );
+	if ($result->numRows() == 0) {
+		
+		require_once(dirname(__FILE__)."/class.editorinfo.php");
+		$ck_info = new editorinfo();
+		
+		$temp = array_keys( $ck_info->toolbars );
+		
+		$fields = array(
+			'editor'	=> "ckeditor_4",
+			'skin'	=> $ck_info->skins[0],
+			'menu'	=> $temp[0],
+			'width'	=> "100%",
+			'height' => "250px"
+		);
+		
+		$database->query( 
+			$database->build_mysql_query(
+				'INSERT',
+				TABLE_PREFIX."mod_wysiwyg_admin",
+				$fields
+			)
+		);
+	}
+}
 ?>
