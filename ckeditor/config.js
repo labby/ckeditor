60c5f17da6fb1650edfa9710e2f065ea08d09e6b
labby <rp@cms-lab.com>
2015-11-05 09:40:08 +0100

    added justify plugin http://forum.lepton-cms.org/viewtopic.php?f=31&t=2660

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -35,4 +35,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	
+	// extraPlugins configuration setting
+	config.extraPlugins = 'justify';
 };
