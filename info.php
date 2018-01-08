3c1b87c76fdfc8e75aba347223dfce20a5029bc3
labby <rp@cms-lab.com>
2018-01-08 10:15:40 +0100

    upate cke to 4.8.0.0

--- info.php
+++ info.php
@@ -31,7 +31,7 @@ if (defined('LEPTON_PATH')) {
 $module_directory	= 'ckeditor';
 $module_name		= 'CKEditor';
 $module_function	= 'WYSIWYG';
-$module_version		= '4.7.3.0';
+$module_version		= '4.8.0.0';
 $module_platform	= '2.x';
 $module_author		= 'erpe, Dietrich Roland Pehlke (Aldus)';
 $module_license		= '<a target="_blank" href="http://www.gnu.org/licenses/lgpl.html">LGPL</a>';
